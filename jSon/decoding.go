7be52ea5bc70f389d78843ce2f006b82ef0cfa02
tambarie <gt.gbaragbo@gmail.com>
2021-08-20 22:27:20 +0100

    json marshalling and unmarshaling + marshalindent

--- jSon/decoding.go
+++ jSon/decoding.go
@@ -1,5 +1,4 @@
 package main
-
 import (
 	"encoding/json"
 	"fmt"
@@ -7,26 +6,95 @@ import (
 )
 
 type UserTwo struct {
-	Id	int
-	Name	string
-	Occupation	string
+	Id int
+	Name string
+	Occupation string
 }
+/*
+var u1 = UserTwo{
+	Id:         1,
+	Name:       "Emmanuel",
+	Occupation: "decadev",
+}
+
+var u2 = []UserTwo{
+	{Id: 1,Name: "Emmanuel",Occupation: "student"},
+	{Id: 2,Name: "tambarie",Occupation: "staff"},
+	{Id: 3,Name: "clinton",Occupation: "senior dev"},
+}
+*/
+//UNMARSHALLING
+var uOne UserTwo
+var uTwo []UserTwo
+
+
+//MARSHAL INDENT
+
+
 
 func main()  {
-	var ul UserTwo
-	data := []byte(`{"Id":1,"Name":"John Doe","Occupation":"student"}`)
-	err := json.Unmarshal(data, &ul)
-	if err != nil{
-		log.Fatal(err)
+
+	//Marshalling
+	//jsonData, err := json.Marshal(u1)
+	//if err != nil{
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(string(jsonData))
+	//
+	//jsonDataTwo, err := json.Marshal(u2)
+	//if err != nil{
+	//	log.Fatal(err)
+	//}
+
+	//fmt.Println(string(jsonDataTwo))
+
+	///////////////////
+	//Unmarshalling
+	data := []byte(`{"Id":1,
+					"Name":"Emmanuel",
+					"Occupation":"decadev"}`)
+	errOne := json.Unmarshal(data,&uOne)
+	if errOne != nil{
+		log.Fatal(errOne)
+	}
+	//fmt.Printf("%+v",uOne)
+
+	dataTwo := []byte(`
+				[
+				{"Id" : 2, "Name": "Roger Roe","Occupation": "driver"},
+				{"Id": 3, "Name": "Lucy Smith","Occupation": "teacher"},
+				{"Id" :4, "Name": "David Brown","Occupation": "Programmer"}
+				]
+			`)
+	errTwo := json.Unmarshal(dataTwo,&uTwo)
+	if errTwo != nil{
+		log.Fatal(errTwo)
 	}
 
-	fmt.Println("Struct is:",ul)
-	fmt.Printf("%s is a %s. \n", ul.Name, ul.Occupation)
+	for _,v := range uTwo{
+		fmt.Printf("%+v",v)
+	}
 
+	//Pretty print using Marshall Indent
+
+	birds := map[string]interface{}{
+		"sounds": map[string]string{
+			"Pigeon": "coo",
+			"eagle": "squak",
+			"owl": "hoot",
+			"duck": "quack",
+			"cuckoo": "ku-ku",
+			"raven": "cruck-cruck",
+			"chicken": "cluck",
+			"rooster": "cock-a-doodle-do",
+		},
+	}
+
+	//dataThree,errThree := json.MarshalIndent(birds, "", "   ")
+	dataThree,errThree := json.Marshal(birds)
+	if errThree != nil{
+		log.Fatal(errThree)
+	}
+	fmt.Println(string(dataThree))
 
 }
-
-
-//{"Id":1,"Name":"John Doe","Occupation":"student"}
-//[{"Id":2,"Name":"Roger Roe","Occupation":"driver"},{"Id":3,"Name":"Lucy Smith","Occupation":"teacher"},{"Id":4,"Name":"David Brown","Occupation":"programmer"}]
-
